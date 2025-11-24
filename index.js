b6c251c3566404ee1d985416bc9b061cf24e640b
Sami Ahmed <saminazirqureshi1@gmail.com>
2025-11-25 03:00:52 +0500

    updated

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ console.log("ENV LOADED:", process.env.REFRESH_EXPIRES_IN);
 const app = express();
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+app.use(cors({
+  origin: "*" // or your frontend URL
+}));
+
+// app.use(express.json());
 app.use(express.json({ strict: false }));
 
-// allow cross-origin requests
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", 
-    "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// // allow cross-origin requests
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", 
+//     "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 // Import Routes
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posts");
@@ -41,8 +45,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected to DB:", mongoose.connection.name);
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
+    const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
   })
   .catch((err) => console.log("DB Error:", err));
